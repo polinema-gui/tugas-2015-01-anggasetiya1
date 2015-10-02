d1f8fb8bc470dce25c3fc89f906d20b71d76aca9
anggasetiya1 <angga.setiya94@gmail.com>
2015-10-02 15:42:44 +0700

    Tugas 1 - versi 1

---
+++ tugas-gui-2/tugas-gui-2/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Klik_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Blue Then
+            Me.BackColor = Color.Pink
+        Else : Me.BackColor = Color.Cyan
+
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Orange
+    End Sub
+End Class
